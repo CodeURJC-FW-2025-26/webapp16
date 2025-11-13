3fbc4c188032b35a83ee03335bb12ab9e91f2a34
Raulgrp7 <ra7garcia@gmail.com>
2025-11-13 11:09:11 +0100

    app.js

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.get("/indice", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "views", "indice.html"));
 });
 
-app.get("/add", (req, res) => {
+app.post("/addFilm", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "views", "add.html"));
 });
 
