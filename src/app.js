936143494bf46268e7a163a8dcc786984c310f50
Raulgrp7 <ra7garcia@gmail.com>
2025-11-13 12:48:11 +0100

    cambio app

--- src/app.js
+++ src/app.js
@@ -2,13 +2,17 @@ import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
 import multer from "multer";
-import databaseRouter from './Database.js';
+import router from './router.js';
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "..", "Public")));
 app.use('/views', express.static(path.join(__dirname, '..', 'views')));
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+// parse application/json
+app.use(express.json());
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "views", "indice.html"));
 });
@@ -17,7 +21,8 @@ app.get("/indice", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "views", "indice.html"));
 });
 
-app.use('/', databaseRouter);
+// Mount main router (contains POST /addFilm)
+app.use('/', router);
 
 app.get("/ej", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "views", "Ej.html"));
