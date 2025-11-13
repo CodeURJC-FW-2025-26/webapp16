fd0d1049fcadfb570f5798a963f330f70ae1bee1
Ignacioddv <ignacioddv@gmail.com>
2025-11-13 13:01:01 +0100

    Agregar los datos de la identidad principal a BD 1

--- src/Database.js
+++ src/Database.js
@@ -16,12 +16,35 @@ const client = new MongoClient(uri);
          description: 'In a future where Earth is slowly dying, humanitys survival depends on venturing into the stars. A former pilot is recruited for a daring mission: to travel through a newly discovered wormhole and search for a new home among the stars.As the crew faces unimaginable challenges across space and time, their journey becomes not only a fight for survival, but also a profound exploration of love, sacrifice, and the very limits of human possibility.',
          Realase_year: '2014', 
          Gender: 'Science fiction', 
-         Calification: 5/5, 
-         Age_classification: +13, 
+         Calification: '5/5', 
+         Age_classification: '+13', 
          Director: 'Christopher Nolan', 
          Casting: 'Matthew McConaughey, Jessica Chastain, Anne Hathaway',
          Duration: '2h 49min'
+        },
+        {
+         Title: 'How to train your dragon',
+         description: 'Hiccup, a teenage Viking, begins dragon training classes, and finally sees an opportunity to prove he is capable of becoming a warrior, when he befriends an injured dragon.',
+         Realase_year: '2010', 
+         Gender: 'Action', 
+         Calification: '4/5', 
+         Age_classification: '+7', 
+         Director: 'Chris Sanders, Dean DeBlois', 
+         Casting: 'Jay Baruchel, Gerard Butler, America Ferrara',
+         Duration: '1h 38min'
+        },
+        {
+         Title: 'The secret live of Walter Mity',
+         description: 'Walter Mitty, photo editor at Life magazine,has spent his whole life taking little "mental vacations" to escape his boring existence, moments in which he becomes the imaginary protagonist of fantastic adventures.',
+         Realase_year: '2013', 
+         Gender: 'Action, comedy', 
+         Calification: '5/5', 
+         Age_classification: '+12', 
+         Director: 'Ben Stiller', 
+         Casting: 'Ben Stiller, Kristen Wiig, Sean Penn',
+         Duration: '1h 54min'
         }
+        
     ]);
 
 export default router;
