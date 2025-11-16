e03a7511a7c49dae49cdd7328652376f11250dd9
Raulgrp7 <ra7garcia@gmail.com>
2025-11-16 13:12:06 +0100

    Realizacion de guardado de comentarios en la base de datos

--- src/models/Comentary.js
+++ src/models/Comentary.js
@@ -3,14 +3,28 @@ import mongoose from 'mongoose';
 const comentarySchema = new mongoose.Schema({
     User_name: {
         type: String,
-        require: true,
-        unique: true
+        required: true
     },
     description: {
         type: String,
         required: true
     },
     Rating: {
-        type: String
+        type: Number,
+        min: 1,
+        max: 10
     },
-})
+    movieId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Movie',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Comentary = mongoose.model('Comentary', comentarySchema, 'comentaries');
+
+export default Comentary;
